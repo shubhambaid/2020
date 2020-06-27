dae00d7678cfbb2db7f87de7da47b5a562d39685
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-06-27 21:50:47 +0530

    Build

---
+++ precache-manifest.193d7b4cd7c72f22687a16d01a7f268e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459793e9657dd6abe32fe8d55364261f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ca780047892427cb108",
+    "url": "/static/css/2.cf98a046.chunk.css"
+  },
+  {
+    "revision": "f2979794fca285ae68bd",
+    "url": "/static/css/main.50da756c.chunk.css"
+  },
+  {
+    "revision": "7ca780047892427cb108",
+    "url": "/static/js/2.4608a802.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4608a802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2979794fca285ae68bd",
+    "url": "/static/js/main.fd9e89e2.chunk.js"
+  },
+  {
+    "revision": "a41498236fbafb992353",
+    "url": "/static/js/runtime-main.eb84fa2e.js"
+  }
+]);
