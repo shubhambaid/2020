340b625b9c01c22b1b7067852c9457cdf0d38c5e
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-07-22 23:46:50 +0530

    Common Navbar for All Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Home from "./components/HomeComp";
+import Navbar from "./components/Navbar";
 import "fdweb/fluent.css";
 import "./App.css";
 
@@ -7,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Navbar />
         <Home />
       </div>
     );
--- src/components/HomeComp.js
+++ src/components/HomeComp.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import Footer from "./FooterComp";
-import Navbar from "./Navbar";
 import Sponsors from "./Sponsors";
 import Prizes from "./Prizes";
 import Cover from "./Cover";
 import Schedule from "./Schedule";
-import About from "./About";
 
 class Home extends Component {
   render() {
@@ -13,7 +11,6 @@ class Home extends Component {
 
       <div>
         <Cover />
-        <Navbar />
         <Prizes />
         <Schedule />
         <Sponsors />
