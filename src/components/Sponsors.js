e3485a1c429470b0b4b774d8732fdc091ec5ba14
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-07-23 12:06:01 +0530

    Sponsors Fixed

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import "../Sponsors.css";
+import devfolio from "../images/sponsors/devfolio.png";
+import matic from "../images/sponsors/matic.png";
+import fold from "../images/sponsors/fold.png";
 
 class Sponsors extends Component {
     render() {
@@ -11,15 +14,42 @@ class Sponsors extends Component {
               <br /><br /><br /><br />
               <div className="column large8 medium8 small12">
                 <h2 className="ta_center">Title</h2>
-                
-              </div><br /><br /><br /><br />
-              <div className="column large8 medium8 small12">
-                <h2 className="ta_center">Gold</h2>
-              </div><br /><br /><br /><br />
-              <div className="column large8 medium8 small12">
-                <h2 className="ta_center">Silver</h2>
-              </div><br /><br /><br /><br />
+                <br />
+                <img src={devfolio} className="center" style={{width: "400px"}} />
+              </div><br /><br /><br /><br /><br /><br />
 
+
+              <div className="column large10 medium12 small12">
+                <h2 className="ta_center">Gold</h2><br />
+                <div className="column large3 medium3 small3">
+                </div>
+                <div className="column large3 medium3 small3">
+                    <img src={fold} className="center" style={{width: "225px"}} />                    
+                </div>
+                <div className="column large3 medium3 small3">
+                    <img src={matic} className="center" style={{width: "225px"}} />                    
+                </div>
+                <div className="column large3 medium3 small3">
+                </div>
+              </div><br /><br /><br /><br /><br /><br />
+
+
+              <div className="column large10 medium12 small12">
+                <h2 className="ta_center">Silver</h2><br />
+                <div className="column large3 medium3 small3">
+                    <img src={fold} className="center" style={{width: "225px"}} />                    
+                </div>
+                <div className="column large3 medium3 small3">
+                    <img src={fold} className="center" style={{width: "225px"}} />                    
+                </div>
+                <div className="column large3 medium3 small3">
+                    <img src={matic} className="center" style={{width: "225px"}} />                    
+                </div>
+                <div className="column large3 medium3 small3">
+                    <img src={fold} className="center" style={{width: "225px"}} />                    
+                </div>
+              </div><br /><br /><br /><br />
+              
               <br /><br /><br /><br /><br /><br />
             </div>
             <br /><br /><br /><br />
