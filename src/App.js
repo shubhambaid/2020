07ac4b6701937feab3bb78dce37944c1117b1078
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-07-24 00:41:41 +0530

    Added Lazy Load

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, Suspense, lazy } from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Home from "./components/HomeComp";
 import Navbar from "./components/Navbar";
 import Teams from "./components/Teams";
-import Speakers from "./components/Speakers"
+import Speakers from "./components/Speakers";
+import Loading from "./components/LoadComp";
 import "fdweb/fluent.css";
 import "fdweb/fluent-icons.css";
 import "./App.css";
+
+const Home = lazy(() =>import("./components/HomeComp"));
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/speakers" component={Speakers} />
-          <Route path="/teams" component={Teams} />
-          <Route path="/coc" component={Home} />
-        </Switch>
-        <Navbar />
+        <Suspense fallback={Loading}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/speakers" component={Speakers} />
+            <Route path="/teams" component={Teams} />
+            <Route path="/coc" component={Home} />
+          </Switch>
+          <Navbar />
+        </Suspense>
       </Router>
     );
   }
