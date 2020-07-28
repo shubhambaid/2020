8cd349575833111cde5945ecb30e67181b8d8a22
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-07-28 22:55:57 +0530

    Working on port 5000

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 8080;
+const express = require("express");
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+const path= require("path");
+var cookieParser = require('cookie-parser');
+
+//process.env
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "client/build")));
+
+if(process.env.NODE_ENV === "production"){
+    //serve static content
+    app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname), "client/build/index.html/");
+})
+
+app.listen(PORT, () =>{
+    console.log("server has started on port " + PORT);
+})
