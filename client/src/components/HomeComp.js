18512532db232885701e69a09c59b9555fd4533a
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-08-11 21:10:03 +0530

    Updated Home Screen Name

--- client/src/components/HomeComp.js
+++ client/src/components/HomeComp.js
@@ -10,14 +10,14 @@ class Home extends Component {
   render() {
 
     const hideloader = () =>{
-      document.getElementById("loadScreen").style.display="none";
+      document.getElementById("loadScreenhome").style.display="none";
     }
   
     return (
 
       <section className="home" onLoad={hideloader}>
 
-        <div id="loadScreen" class="modal_container" style={{display: "block", background: "#2e2e2eee",  backdropFilter: "blur(20px)"}}>
+        <div id="loadScreenhome" class="modal_container" style={{display: "block", background: "#2e2e2eee",  backdropFilter: "blur(20px)"}}>
             <div class="center middle">
                 <div class="simple_progress_bar"></div>
             </div>
