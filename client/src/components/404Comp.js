798fce253a518c094644300caaecf918660e87e7
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-08-13 23:08:57 +0530

    Updated 404

--- client/src/components/404Comp.js
+++ client/src/components/404Comp.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react"
-import "./extra.css";
 
 class FourZeroFour extends Component{
 
@@ -26,8 +25,8 @@ class FourZeroFour extends Component{
           <h1 style={{fontSize: "20vw"}} className="small0 ta_center"><span className="type">404</span></h1>
           <h1 style={{fontSize: "20vh"}} className="large0 medium0 ta_center"><span className="type">404</span></h1>
           <p className="ta_center">
-            <h2 className="ta_center">We Couldn't Connect The Dots</h2>
-            The page you want to access doesn't exist or you assembled the link incorrectly
+            <h2 className="ta_center">We Couldn't Connect The Dots</h2><br /><br />
+            <span style={{fontSize: '20px', textAlign: "center", display: 'block'}} className="ta_center">The page you want to access doesn't exist or you assembled the link incorrectly</span>
           </p><br />
           <button className="large frost_container primary_blue center" onMouseOver={this.frosting} onClick={this.openhome}>
             <span className="frost">Head To Home</span>
