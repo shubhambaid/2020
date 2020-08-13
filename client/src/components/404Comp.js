b1bdec339d675ff50c509e51f20abd4c39573d59
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-08-14 00:04:56 +0530

    Updated 404

--- client/src/components/404Comp.js
+++ client/src/components/404Comp.js
@@ -21,7 +21,11 @@ class FourZeroFour extends Component{
   
   render(){
     return(
-      <div className="page_container">
+      <div style={{overflowY: "hidden"}}>
+        <button className="large frost_container primary_blue center" onMouseOver={this.frosting} onClick={this.openhome} style={{position: "fixed", bottom: "18%", left: "45vw", zIndex: "20"}}>
+          <span className="frost">Head To Home</span>
+        </button>
+        <img src={Four0Four} alt="" style={{height: "99.5vh", width: "100%", overflow: "hidden"}} />
         <div className="column large0 medium0 small0 center">
           <h1 style={{fontSize: "20vw"}} className="small0 ta_center"><span className="type">404</span></h1>
           <h1 style={{fontSize: "20vh"}} className="large0 medium0 ta_center"><span className="type">404</span></h1>
@@ -29,12 +33,11 @@ class FourZeroFour extends Component{
             <h2 className="ta_center">We Couldn't Connect The Dots</h2><br /><br />
             <span style={{fontSize: '20px', textAlign: "center", display: 'block'}} className="ta_center">The page you want to access doesn't exist or you assembled the link incorrectly</span>
           </p><br />
+          <br /><br />
           <button className="large frost_container primary_blue center" onMouseOver={this.frosting} onClick={this.openhome}>
             <span className="frost">Head To Home</span>
           </button>
-          <br /><br />
         </div>
-        <img src={Four0Four} alt="" />
       </div>
     );
   }
