1ccac20f1271e3e23066182b6609ff6ead5a79c0
Dharshan K <dharshan0011@gmail.com>
2020-08-18 17:53:46 +0530

    added discord btn

--- client/src/components/Cover.js
+++ client/src/components/Cover.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../css/Cover.css";
+import discord from "../images/discord.svg";
 
 function Cover(props) {
   // const [mode, setMode] = useState("dark");
@@ -41,29 +42,34 @@ function Cover(props) {
       </div> */}
 
       {/* {isMobile? <img src={coverImageMobile} alt="" /> : <img src={coverImageLarge} alt="" />} */}
-      <div style={{position: "relative", top: "20vh"}}>
-      <h1>
-        REVA HACK &#60;&#47;&#62;<sup>20</sup>
-        <sub>20</sub>
-      </h1>
-      <br />
-      <hr />
-      <br />
-      <h4>Code for Change</h4>
-      <p>04-07 November</p>
-      <button id="devfolio-apply-now" className="devfolio-btn">
-        <svg
-          class="logo"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="#fff"
-          viewBox="0 0 115.46 123.46"
-          style={{ height: "24px", width: "24px", marginRight: "8px" }}
-        >
-          <path d="M115.46 68a55.43 55.43 0 0 1-50.85 55.11S28.12 124 16 123a12.6 12.6 0 0 1-10.09-7.5 15.85 15.85 0 0 0 5.36 1.5c4 .34 10.72.51 20.13.51 13.82 0 28.84-.38 29-.38h.26a60.14 60.14 0 0 0 54.72-52.47c.05 1.05.08 2.18.08 3.34z" />
-          <path d="M110.93 55.87A55.43 55.43 0 0 1 60.08 111s-36.48.92-48.58-.12C5 110.29.15 104.22 0 97.52l.2-83.84C.38 7 5.26.94 11.76.41c12.11-1 48.59.12 48.59.12a55.41 55.41 0 0 1 50.58 55.34z" />
-        </svg>
-        Apply with Devfolio
-      </button>
+      <div style={{ position: "relative", top: "20vh" }}>
+        <h1>
+          REVA HACK &#60;&#47;&#62;<sup>20</sup>
+          <sub>20</sub>
+        </h1>
+        <br />
+        <hr />
+        <br />
+        <h4>Code for Change</h4>
+        <p>04-07 November</p>
+        <button id="devfolio-apply-now" className="devfolio-btn">
+          <svg
+            class="logo"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#fff"
+            viewBox="0 0 115.46 123.46"
+            style={{ height: "24px", width: "24px", marginRight: "8px" }}
+          >
+            <path d="M115.46 68a55.43 55.43 0 0 1-50.85 55.11S28.12 124 16 123a12.6 12.6 0 0 1-10.09-7.5 15.85 15.85 0 0 0 5.36 1.5c4 .34 10.72.51 20.13.51 13.82 0 28.84-.38 29-.38h.26a60.14 60.14 0 0 0 54.72-52.47c.05 1.05.08 2.18.08 3.34z" />
+            <path d="M110.93 55.87A55.43 55.43 0 0 1 60.08 111s-36.48.92-48.58-.12C5 110.29.15 104.22 0 97.52l.2-83.84C.38 7 5.26.94 11.76.41c12.11-1 48.59.12 48.59.12a55.41 55.41 0 0 1 50.58 55.34z" />
+          </svg>
+          Apply with Devfolio
+        </button>
+        <a href="https://discord.com/" className="discord">
+          <span>Join Us On</span>
+
+          <img src={discord} alt="discord" />
+        </a>
       </div>
     </div>
   );
