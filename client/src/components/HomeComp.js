089fab190347f654c9645aba21abd4a478a9802b
Dharshan K <dharshan0011@gmail.com>
2020-08-29 13:32:53 +0530

    added tracks

--- client/src/components/HomeComp.js
+++ client/src/components/HomeComp.js
@@ -5,34 +5,39 @@ import Prizes from "./Prizes";
 import Cover from "./Cover";
 import About from "./About";
 import Schedule from "./Schedule";
+import Tracks from "./Tracks";
 
 class Home extends Component {
   render() {
+    const hideloader = () => {
+      document.getElementById("loadScreenhome").style.display = "none";
+    };
 
-    const hideloader = () =>{
-      document.getElementById("loadScreenhome").style.display="none";
-    }
-  
     return (
-
       <section className="home" onLoad={hideloader}>
-
-        <div id="loadScreenhome" class="modal_container" style={{display: "block", background: "#2e2e2eee",  backdropFilter: "blur(20px)"}}>
-            <div class="center middle">
-                <div class="simple_progress_bar"></div>
-            </div>
+        <div
+          id="loadScreenhome"
+          class="modal_container"
+          style={{
+            display: "block",
+            background: "#2e2e2eee",
+            backdropFilter: "blur(20px)",
+          }}
+        >
+          <div class="center middle">
+            <div class="simple_progress_bar"></div>
+          </div>
         </div>
-
 
         <div>
           <Cover />
           <About />
+          <Tracks />
           <Prizes />
           <Schedule />
           <Sponsors />
           <Footer />
         </div>
-
       </section>
     );
   }
