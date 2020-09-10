794cb8e1b7a6588f0f82d13ff34c022140ba6931
Ava68 <63304698+Ava68@users.noreply.github.com>
2020-09-11 01:08:23 +0530

    padding fixed

--- client/src/components/HomeComp.js
+++ client/src/components/HomeComp.js
@@ -35,7 +35,7 @@ class Home extends Component {
           <About />
           <Tracks />
           <Prizes />
-          <Bounties/>
+          <Bounties />
           <Schedule />
           <Sponsors />
           <Footer />
