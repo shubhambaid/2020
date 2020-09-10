1cbbf5936c1466b27329cdd3335193eefefa89f7
deepvashisth <deepvashisth7777@gmail.com>
2020-09-11 00:43:32 +0530

    padding updated

--- client/src/components/Prizes.js
+++ client/src/components/Prizes.js
@@ -5,22 +5,17 @@ import "../css/Prizes.css";
 class Prizes extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div>
-        <div className="primary_inverted toop">
-=======
       <div className="prizes">
-        <div className="primary_inverted" style={{padding: "0px 10px"}}>
->>>>>>> 169fd9816e9e1d94a1f5acbf1dd34c9d629b2ea7
-        <br /><br /><br /><br /><br /><br />
+        <div className="primary_inverted" style={{ padding: "0px 10px" }}>
+          <br /><br /><br /><br /><br /><br />
           <h1 className="ta_center title">Prizes and Incentives</h1>
           <br /><br /><br /><br />
           <div className="column large8 medium8 small12">
             <h3 className="ta_center">1st Prize</h3>
             <h1 className="ta_center">INR 25,000</h1><br /><br /><hr className="large8 medium8 small10" /><br /><br />
           </div><br /><br /><br /><br />
-          <div className="column large6 medium6 small12" style={{borderRight: "1px solid #fcfcfc"}}>
-          <br /><br />
+          <div className="column large6 medium6 small12" style={{ borderRight: "1px solid #fcfcfc" }}>
+            <br /><br />
             <h6 className="ta_center">2nd Prize</h6>
             <h3 className="ta_center">INR 15,000</h3><br /><br />
           </div>
