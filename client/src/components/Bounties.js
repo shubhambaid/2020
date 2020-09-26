cde1defa5ffe6bc5fb94fef4ca436772050907ed
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-09-26 14:35:22 +0530

    Added Title to Bounties

--- client/src/components/Bounties.js
+++ client/src/components/Bounties.js
@@ -6,7 +6,7 @@ import portis from "../images/bounties/portis.svg";
 function Bounties() {
   return (
     <div className="bounties">
-      <h1>Bounties</h1>
+      <h1 className="title">Bounties</h1>
       <div>
         <img src={portis} alt="bounties" />
         <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/11lWtlNXmNN4pCjIv8s1xdS5JVmk0KMvf/view?usp=sharing">Know More</a>
